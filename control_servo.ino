80a0fa330f23f939016ad44484ddbe4d2d0b33b2
Engine Beast <131536099+enginebeast@users.noreply.github.com>
2025-08-21 22:28:36 +0900

    Add files via upload

---
+++ control_servo.ino
@@ -0,0 +1,40 @@
+#include <Servo.h>
+
+Servo servo1;
+char button;
+void setup(){
+  Serial.begin(9600);
+  servo1.attach(8, 500, 2500);
+  servo1.writeMicroseconds(1500);
+}
+
+void loop(){
+   if (Serial.available()) {
+    button = Serial.read();
+  }
+    
+  if (button  == 'a'){
+    servo1.writeMicroseconds(2500);
+    delay(500);
+  }
+  
+  else if (button  == 's'){
+    servo1.writeMicroseconds(2000);
+    delay(500);
+  }
+
+  else if (button  == 'd'){
+    servo1.writeMicroseconds(1500);
+    delay(500);
+  }
+
+  else if (button  == 'f'){
+    servo1.writeMicroseconds(1000);
+    delay(500);
+  }
+
+  else if (button  == 'g'){
+    servo1.writeMicroseconds(500);
+    delay(500);
+  }
+}
